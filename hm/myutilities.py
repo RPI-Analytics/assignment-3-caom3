6c82b4ac451dda02c490772fc0cabe308abfa4e2
Jessie <cao_mq@yeah.net>
2018-02-12 22:59:00 -0500

    HW03

---
+++ hm/myutilities.py
@@ -0,0 +1,12 @@
+def divby2(b):
+    result = []
+    for i in b:
+        if i%2==0:
+            result.append(i)
+    return result
+def divby3(b):
+    result = []
+    for i in b:
+        if i%3==0:
+            result.append(i)
+    return result
